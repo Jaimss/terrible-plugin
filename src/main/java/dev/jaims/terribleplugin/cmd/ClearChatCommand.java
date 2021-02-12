bf9158c17e88fc13266be7837df5084423d3781a
Kqliber <Dylansaidler03@gmail.com>
2021-02-12 20:56:08 +0000

    clear chat

---
+++ src/main/java/dev/jaims/terribleplugin/cmd/ClearChatCommand.java
@@ -0,0 +1,88 @@
+package dev.jaims.terribleplugin.cmd;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+@SuppressWarnings("unused")
+public class ClearChatCommand implements CommandExecutor {
+
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("terribleplugin.clearchat")) {
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+            Bukkit.broadcastMessage("");
+
+            Bukkit.broadcastMessage(ChatColor.STRIKETHROUGH + "-------------------------------");
+            Bukkit.broadcastMessage(ChatColor.BLUE + "Chat Cleared by" + " " + ChatColor.AQUA + sender.getName());
+
+
+        }
+        return true;
+    }
+
+
+
+}
