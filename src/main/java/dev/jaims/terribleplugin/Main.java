23b8334ec64b903d280c218fd9eec4e02b5b22cd
Fefo6644 <federico.lopez.1999@outlook.com>
2021-02-12 16:45:32 -0300

    add command and permission

--- src/main/java/dev/jaims/terribleplugin/Main.java
+++ src/main/java/dev/jaims/terribleplugin/Main.java
@@ -1,14 +1,15 @@
 package dev.jaims.terribleplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
 public class Main extends JavaPlugin {
+    public void onLoad() {    }
 
-    @Override
-    public void onLoad() {}
-
-    @Override
     public void onEnable() {
         for (int i = 0; i < 10; i ++) {
             System.out.println(" ");
@@ -20,6 +21,12 @@ public class Main extends JavaPlugin {
             System.out.println(" ");
         }
     }
-    @Override
     public void onDisable() {}
+
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (command.getName() == "command") {
+            ((Player) sender).sendMessage("§eHello " + ChatColor.GOLD + ":D");
+        }
+        return false;
+    }
 }
