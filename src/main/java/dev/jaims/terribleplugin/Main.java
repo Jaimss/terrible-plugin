35bdb512e7b94d56d9c78b4409da1a24bce5bd1c
jaimss <james@harrell365.com>
2021-02-12 14:31:45 -0500

    main, plugin.yml

--- src/main/java/dev/jaims/terribleplugin/Main.java
+++ src/main/java/dev/jaims/terribleplugin/Main.java
@@ -1,4 +1,19 @@
 package dev.jaims.terribleplugin;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(" ");
+        }
+        System.out.println("The worst plugin is loading!");
+
+        System.out.println("The worst plugin has loaded!");
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(" ");
+        }
+    }
 }
