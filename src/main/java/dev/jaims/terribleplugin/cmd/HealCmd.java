969e1c54065461ae24dcbe373546b549a75287b2
thienbao860 <35884937+thienbao860@users.noreply.github.com>
2021-02-14 20:44:24 +0700

    Create HealCmd.java

---
+++ src/main/java/dev/jaims/terribleplugin/cmd/HealCmd.java
@@ -0,0 +1,46 @@
+package dev.jaims.terribleplugin.cmd;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.*;
+
+public class HealCmd implements CommandExecutor {
+
+    public static String green = "\033[0;32m";
+
+    //sorry, im a lua addictor
+    public static long index = 1;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command commmand, String label, String[] args) {
+
+        Player p= (Player)sender;
+
+        if (commmand.getName().equalsIgnoreCase("heal")== Boolean.TRUE||commmand.getName().equalsIgnoreCase("restore") == Boolean.TRUE)
+            if (p.hasPermission((String)"terribleplugin.heal") ||p.hasPermission((String) "*")|| p.isOp() == Boolean.TRUE)
+                try {
+                    Player t =Objects.requireNonNull(Bukkit.getPlayer(args[index - 1]));
+
+                    try {
+                        Double d=new Double(args[2]);
+                        if (d.isNaN() !=Boolean.TRUE && d.isInfinite()!=Boolean.TRUE) {
+                            t.setHealth(d.doubleValue());
+                            p.sendMessage(green + "Ths player have been healthy to " + String.format("%d", d.doubleValue()) + " health");
+                            t.sendMessage(green + "You have been healthy, gg boi");
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        t.sendMessage("Bad number!");
+                    }
+                }
+                catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+        return false;
+    }
+}
