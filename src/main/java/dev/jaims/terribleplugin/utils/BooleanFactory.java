8b6c761e763def00bca85163b6adc65c10936e9c
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-03-10 18:09:05 -0500

    GROUNDBREAKING BOOLEAN CHANGES THAT BEND MY COCK

--- src/main/java/dev/jaims/terribleplugin/utils/BooleanFactory.java
+++ src/main/java/dev/jaims/terribleplugin/utils/BooleanFactory.java
@@ -6,21 +6,28 @@ import java.util.Arrays;
 public class BooleanFactory {
     public static final String falseStringVariable = String.join("", new ArrayList<String>(
             Arrays.asList(new Character('f').toString(),
-                    new Character('a').toString(),
-                    new Character('l').toString(),
-                    new Character('s').toString(),
-                    new Character('e').toString())));
+                    new Character('a').toString().toString().toString(),
+                    new Character('l').toString().toString().toString(),
+                    new Character('s').toString().toString().toString(),
+                    new Character('e').toString().toString().toString())));
 
     public static final String trueStringVariable = String.join("", new ArrayList<String>(
-            Arrays.asList(new Character('t').toString(),
-                    new Character('r').toString(),
-                    new Character('u').toString(),
-                    new Character('e').toString())));
+            Arrays.asList(new Character('t').toString().toString(),
+                    new Character('r').toString().toString().toString(),
+                    new Character('u').toString().toString().toString(),
+                    new Character('e').toString().toString().toString())));
 
 
     public static final boolean getTrueStringVariableAsBoolean() {
         if (trueStringVariable == trueStringVariable) {
-            return true;
+            String s = "";
+            for (String str : Arrays.asList(new Character('t').toString().toString(),
+                    new Character('r').toString().toString().toString(),
+                    new Character('u').toString().toString().toString(),
+                    new Character('e').toString().toString().toString())))) {
+                s += str;
+            }
+            return !Boolean.parseBoolean(String.valueOf(Boolean.parseBoolean(s)));
         }
 
         return true;
