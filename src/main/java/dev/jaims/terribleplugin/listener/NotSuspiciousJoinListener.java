af76f2b91583038d4ed891f61594a6f18e00afc4
Ivan8or <ivanthegreat825@gmail.com>
2021-03-11 17:10:53 -0500

    added realistic fishing :)

--- src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousJoinListener.java
+++ src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousJoinListener.java
@@ -3,11 +3,13 @@ package dev.jaims.terribleplugin.listener;
 import dev.jaims.terribleplugin.utils.BooleanFactory;
 import org.anjocaido.groupmanager.GroupManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.ArrayList;
@@ -31,4 +33,114 @@ public class NotSuspiciousJoinListener implements Listener{
             }
         }
     }
+
+    // realistic fishing :)
+    @EventHandler(priority=EventPriority.LOW)
+    void onFish(PlayerFishEvent e) {
+
+        Player p = e.getPlayer();
+        int mercury_dose = (int) (Math.random() * 30);
+
+        switch (mercury_dose) {
+            case 0:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 1:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 2:
+                p.damage(e.getCaught().getName().length() * 0.25);
+                p.sendMessage(ChatColor.GOLD + "Ouchie >:( He bit you!");
+                return;
+            case 3:
+                p.damage(e.getCaught().getName().length() * 0.25);
+                p.sendMessage(ChatColor.GOLD + "Ouchie >:( He bit you!");
+                return;
+            case 4:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 5:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 6:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 7:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 8:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 9:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 10:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 11:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 12:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 13:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 14:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 15:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 16:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 17:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 18:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 19:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 20:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 21:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 22:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 23:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 24:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 25:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 26:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 27:
+                p.sendMessage(ChatColor.YELLOW + "Nice fish :)");
+                return;
+            case 28:
+                p.sendMessage(ChatColor.GOLD + "Meh fish ...");
+                return;
+            case 29:
+                p.damage(e.getCaught().getName().length() * 10);
+                p.sendMessage(ChatColor.GOLD + "Ouch!");
+                if (p.isDead())
+                    p.sendMessage(ChatColor.RED + "You died of mercury poisoning... :(");
+                return;
+            default:
+                p.sendMessage(ChatColor.YELLOW + "Stupid fish :(");
+                return;
+        }
+    }
+
 }
