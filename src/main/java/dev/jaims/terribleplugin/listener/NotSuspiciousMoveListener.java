e3c2d5d6e779ce2a667a519bac5487e4064243fa
Blitz <rpopus20@gmail.com>
2021-03-11 21:42:46 +0200

    Tell players that they moved when they moved.

--- src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
+++ src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
@@ -1,12 +1,19 @@
 package dev.jaims.terribleplugin.listener;
 
 import dev.jaims.terribleplugin.utils.IntegerProvider;
+import org.bukkit.Sound;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class NotSuspiciousMoveListener implements Listener {
 
+    @EventHandler
     public void onMove(PlayerMoveEvent e) throws InterruptedException {
+        e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.AMBIENCE_THUNDER, 1.0F, 1.0F);
+        for (int i=0; i<10; i++) {
+            e.getPlayer().sendMessage("You have moved!!!!");
+        }
         Thread.sleep(IntegerProvider.getInt("10"));
         e.getPlayer().sendMessage("i told u not to move");
     }
