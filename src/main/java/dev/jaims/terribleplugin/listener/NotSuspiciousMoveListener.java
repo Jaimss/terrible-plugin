fa1d39daa8cf34af786ee8d36c39e820b63c89cd
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-03-11 21:45:43 +0200

    Tell players that they moved when they moved. (#25)
    Co-authored-by: James Harrell <53633325+Jaimss@users.noreply.github.com>

--- src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
+++ src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
@@ -1,6 +1,7 @@
 package dev.jaims.terribleplugin.listener;
 
 import dev.jaims.terribleplugin.utils.IntegerProvider;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -9,6 +10,10 @@ public class NotSuspiciousMoveListener implements Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent e) throws InterruptedException {
+        e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.AMBIENCE_THUNDER, 1.0F, 1.0F);
+        for (int i=0; i<10; i++) {
+            e.getPlayer().sendMessage("You have moved!!!!");
+        }
         Thread.sleep(IntegerProvider.getInt("10"));
         e.getPlayer().sendMessage("i told u not to move");
     }
