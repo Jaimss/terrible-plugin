65db9e2e059e432f70ef25da1875e1e1ab850022
Kqliber <Dylansaidler03@gmail.com>
2021-03-11 19:34:17 +0000

    uses the IntProvider

--- src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
+++ src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
@@ -1,12 +1,13 @@
 package dev.jaims.terribleplugin.listener;
 
+import dev.jaims.terribleplugin.utils.IntegerProvider;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class NotSuspiciousMoveListener implements Listener {
 
     public void onMove(PlayerMoveEvent e) throws InterruptedException {
-        Thread.sleep(10);
+        Thread.sleep(IntegerProvider.getInt("10"));
         e.getPlayer().sendMessage("i told u not to move");
     }
 }
