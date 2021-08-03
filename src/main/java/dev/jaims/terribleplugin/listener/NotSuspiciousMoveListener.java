bd84e1cc4e11d63e1b4f2c816b9f37b75e9259b3
Hannes Greule <SirYwell@users.noreply.github.com>
2021-08-03 16:51:36 +0200

    Make code better (#31)

--- src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
+++ src/main/java/dev/jaims/terribleplugin/listener/NotSuspiciousMoveListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class NotSuspiciousMoveListener implements Listener {
 
     @EventHandler
-    public void onMove(PlayerMoveEvent e) throws InterruptedException {
+    public void onMove(NotSuspiciousMoveListener this, PlayerMoveEvent e) throws InterruptedException {
         e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.AMBIENCE_THUNDER, 1.0F, 1.0F);
         for (int i=0; i<10; i++) {
             e.getPlayer().sendMessage("You have moved!!!!");
