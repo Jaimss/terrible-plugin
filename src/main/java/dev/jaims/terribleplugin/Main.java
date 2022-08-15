7a08342648a4db03d12592744b11d731525ed8dc
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2022-08-15 14:25:25 +0200

    Imports

--- src/main/java/dev/jaims/terribleplugin/Main.java
+++ src/main/java/dev/jaims/terribleplugin/Main.java
@@ -23,6 +23,7 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Random;
 import java
         .lang
         .reflect
